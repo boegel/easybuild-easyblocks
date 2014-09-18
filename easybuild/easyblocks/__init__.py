d79ee20755da74de6daa4f57b6c6cbb73682da8a
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-09-18 13:11:27 +0200

    only extend __path__ with existing easybuild.easyblocks subdirs

--- easybuild/easyblocks/__init__.py
+++ easybuild/easyblocks/__init__.py
@@ -64,7 +64,7 @@ else:
 
 # Extend path so python finds our easyblocks in the subdirectories where they are located
 subdirs = [chr(l) for l in range(ord('a'), ord('z') + 1)] + ['0']
-__path__.extend([os.path.join(__path__[0], subdir) for subdir in subdirs])
+__path__.extend([os.path.join(__path__[0], subdir) for subdir in subdirs if os.path.isdir(os.path.join(__path__[0], subdir))])
 # And let python know this is not the only place to look for them, so we can have multiple
 # easybuild/easyblock paths in your python search path, next to the official easyblocks distribution
 __path__ = extend_path(__path__, __name__)  # @ReservedAssignment
