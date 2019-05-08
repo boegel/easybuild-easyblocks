a474972eefcb3bb0e682a7c0af1f771a350fb6fd
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-05-08 15:36:13 +0200

    cleanup numpy to (consistently) use change_dir and remove_dir (instead of rmtree2)

--- easybuild/easyblocks/n/numpy.py
+++ easybuild/easyblocks/n/numpy.py
@@ -42,7 +42,7 @@ from easybuild.easyblocks.generic.fortranpythonpackage import FortranPythonPacka
 from easybuild.easyblocks.generic.pythonpackage import det_pylibdir
 from easybuild.framework.easyconfig import CUSTOM
 from easybuild.tools.build_log import EasyBuildError
-from easybuild.tools.filetools import change_dir, mkdir, rmtree2
+from easybuild.tools.filetools import change_dir, mkdir, remove_dir
 from easybuild.tools.modules import get_software_root
 from easybuild.tools.run import run_cmd
 from distutils.version import LooseVersion
@@ -225,11 +225,7 @@ class EB_numpy(FortranPythonPackage):
         cmd = self.compose_install_command(tmpdir, extrapath=pythonpath)
         run_cmd(cmd, log_all=True, simple=True, verbose=False)
 
-        try:
-            pwd = os.getcwd()
-            os.chdir(tmpdir)
-        except OSError as err:
-            raise EasyBuildError("Faild to change to %s: %s", tmpdir, err)
+        pwd = change_dir(tmpdir)
 
         # evaluate performance of numpy.dot (3 runs, 3 loops each)
         size = 1000
@@ -268,26 +264,20 @@ class EB_numpy(FortranPythonPackage):
         else:
             raise EasyBuildError("Time for %dx%d matrix dot product: %d msec >= %d msec => ERROR",
                                  size, size, time_msec, self.cfg['blas_test_time_limit'])
-        try:
-            os.chdir(pwd)
-            rmtree2(tmpdir)
-        except OSError as err:
-            raise EasyBuildError("Failed to change back to %s: %s", pwd, err)
+
+        change_dir(pwd)
+        remove_dir(tmpdir)
 
     def install_step(self):
         """Install numpy and remove numpy build dir, so scipy doesn't find it by accident."""
         super(EB_numpy, self).install_step()
 
-        builddir = os.path.join(self.builddir, "numpy")
-        try:
-            if os.path.isdir(builddir):
-                os.chdir(self.builddir)
-                rmtree2(builddir)
-            else:
-                self.log.debug("build dir %s already clean" % builddir)
-
-        except OSError as err:
-            raise EasyBuildError("Failed to clean up numpy build dir %s: %s", builddir, err)
+        builddir = os.path.join(self.builddir, 'numpy')
+        if os.path.isdir(builddir):
+            change_dir(self.builddir)
+            remove_dir(builddir)
+        else:
+            self.log.debug("build dir %s already clean", builddir)
 
     def run(self):
         """Install numpy as an extension"""
