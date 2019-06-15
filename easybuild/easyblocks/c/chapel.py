c8e17b20b40ddbdcb9fd06f57c21e47508817bd7
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-06-15 23:28:20 +0200

    update & enhance Chapel easyblock

--- easybuild/easyblocks/c/chapel.py
+++ easybuild/easyblocks/c/chapel.py
@@ -1,4 +1,4 @@
-## 
+##
 # This file is an EasyBuild reciPY as per https://github.com/easybuilders/easybuild
 #
 # Copyright:: Copyright 2012-2019 Uni.Lu/LCSB, NTUA
@@ -16,9 +16,11 @@ EasyBuild support for Chapel, implemented as an easyblock
 @author: Kenneth Hoste (Ghent University)
 """
 import os
-import shutil
 
 from easybuild.easyblocks.generic.configuremake import ConfigureMake
+from easybuild.tools.modules import get_software_root
+from easybuild.tools.run import run_cmd
+from easybuild.tools.systemtools import get_cpu_architecture
 
 
 class EB_Chapel(ConfigureMake):
@@ -28,12 +30,29 @@ class EB_Chapel(ConfigureMake):
         """Initialize Chapel-specific variables."""
         super(EB_Chapel, self).__init__(*args, **kwargs)
         self.build_in_installdir = True
+        self.setup_script = os.path.join('util', 'setchplenv.bash')
+
+    def extract_step(self):
+        """Extract Chapel source tarball, strip off top-level component."""
+        self.cfg['unpack_options'] = '--strip-components 1'
+
+        super(EB_Chapel, self).extract_step()
 
     def configure_step(self):
         """No configure step for Chapel."""
-        pass
+        self.cfg['prebuildopts'] = "source %s && " % os.path.join(self.installdir, self.setup_script)
 
-    # building is done via make, so taken care of by ConfigureMake easyblock
+        # Chapel requires clang/AST/GlobalDecl.h (so it needs Clang rather than only LLVM)
+        if get_software_root('Clang'):
+            self.cfg.update('prebuildopts', "export CHPL_LLVM=system && ")
+
+        if get_software_root('GMP'):
+            self.cfg.update('prebuildopts', "export CHPL_GMP=system && ")
+
+    def test_step(self):
+        """Run Chapel tests."""
+        self.cfg.update('pretestopts', self.cfg['prebuildopts'])
+        run_cmd("%s CHPL_CHECK_DEBUG=1 make check %s" % (self.cfg['pretestopts'], self.cfg['testopts']))
 
     def install_step(self):
         """Installation of Chapel has already been done as part of the build procedure"""
@@ -42,22 +61,34 @@ class EB_Chapel(ConfigureMake):
     def sanity_check_step(self):
         """Custom sanity check for Chapel."""
 
-        libpath = os.path.join('lib', 'linux64', 'gnu', 'comm-none', 'substrate-none', 'seg-none',
-                               'mem-default', 'tasks-fifo', 'threads-pthreads', 'atomics-intrinsics')
         custom_paths = {
-                        'files': ['bin/linux64/chpl', 'bin/linux64/chpldoc',
-                                  os.path.join(libpath, 'libchpl.a'), os.path.join(libpath, 'main.o')],
-                        'dirs': []
-                       }
+            'files': ['bin/linux64-%s/chpl' % get_cpu_architecture(), self.setup_script],
+            'dirs': [],
+        }
 
-        super(EB_Chapel, self).sanity_check_step(custom_paths=custom_paths)
+        hello = os.path.join(self.builddir, 'hello')
+        custom_commands = [' && '.join([
+            "chpl -o %s %s" % (hello, os.path.join('$EBROOTCHAPEL', 'examples', 'hello.chpl')),
+            hello,
+        ])]
+
+        super(EB_Chapel, self).sanity_check_step(custom_paths=custom_paths, custom_commands=custom_commands)
 
     def make_module_req_guess(self):
         """
         A dictionary of possible directories to look for; this is needed since bin/linux64 of chapel is non standard
         """
         return {
-                'PATH': ['bin', 'bin/linux64', 'bin64'],
-                'LD_LIBRARY_PATH': ['lib', 'lib/linux64', 'lib64'],
-               }
+            'MANPATH': ['man'],
+            'PATH': ['bin/linux64-%s' % get_cpu_architecture()],
+        }
 
+    def make_module_extra(self):
+        """
+        Extra Chapel-specific environment variables to set.
+        """
+        txt = super(EB_Chapel, self).make_module_extra()
+
+        txt += self.module_generator.set_environment('CHPL_HOME', self.installdir)
+
+        return txt
